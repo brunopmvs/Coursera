e9b9ff1cd2ee11b5dd3f50a88278ced76c337020
brunopmvs <brunop@hotmail.com.br>
2020-06-24 16:50:21 -0700

    semForComTeste

---
+++ ICC_USP_2/semana1/atividade1.py
@@ -0,0 +1,28 @@
+"""
+Escreva uma função dimensoes(matriz) que recebe uma matriz como parâmetro
+ e imprime as dimensões da matriz recebida, no formato iXj.
+
+Exemplos:
+minha_matriz = [[1], [2], [3]]
+dimensoes(minha_matriz)
+3X1
+
+minha_matriz = [[1, 2, 3], [4, 5, 6]]
+dimensoes(minha_matriz)
+2X3
+"""
+
+def dimensoes(matriz):
+    """Recebe uma matriz como parâmetro
+    e imprime as dimensões da matriz recebida, no formato iXj."""
+    i = len(matriz)
+    j = len(matriz[0])
+
+    dimensoes = str(i) + "X" + str(j)
+    return dimensoes
+
+# minha_matriz = [[1], [2], [3]]
+# print(dimensoes(minha_matriz))
+
+# minha_matriz = [[1, 2, 3], [4, 5, 6]]
+# print(dimensoes(minha_matriz))
